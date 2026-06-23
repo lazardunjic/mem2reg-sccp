ccfeb2d7eddea88710a886a41445f612c8096d57
Luka Nedeljković <luka.nedeljkovic091@gmail.com>
2026-06-23 22:59:37 +0200

    infra_init

--- src/sccp/CustomSCCP.cpp
+++ src/sccp/CustomSCCP.cpp
@@ -70,4 +70,76 @@ static bool meet(LatticeValue &Into, const LatticeValue &Other) {
     return true;
 }
 
+using CFGEdge = std::pair<BasicBlock *, BasicBlock *>;
+
+struct SCCPSolver {
+
+    DenseMap<Value *, LatticeValue> ValueState;
+
+    DenseSet<CFGEdge>    ExecutableEdges;
+    DenseSet<BasicBlock *> ExecutableBlocks;
+
+    SmallVector<CFGEdge, 64>      CFGWorklist;
+
+    SmallVector<Instruction *, 64> SSAWorklist;
+
+
+    LatticeValue &getValueState(Value *V) {
+        auto It = ValueState.find(V);
+        if (It != ValueState.end())
+            return It->second;
+
+        if (auto *C = dyn_cast<Constant>(V)) {
+            ValueState[V] = LatticeValue::makeConstant(C);
+            return ValueState[V];
+        }
+
+        ValueState[V] = LatticeValue::makeUndef();
+        return ValueState[V];
+    }
+
+    bool markEdgeExecutable(BasicBlock *From, BasicBlock *To) {
+        if (!ExecutableEdges.insert({From, To}).second)
+            return false;                        
+        ExecutableBlocks.insert(To);
+        CFGWorklist.push_back({From, To});
+        return true;
+    }
+
+    bool isEdgeExecutable(BasicBlock *From, BasicBlock *To) const {
+        return ExecutableEdges.count({From, To}) > 0;
+    }
+
+    bool isBlockExecutable(BasicBlock *BB) const {
+        return ExecutableBlocks.count(BB) > 0;
+    }
+
+    void updateState(Instruction *I, LatticeValue NewVal) {
+        LatticeValue &Old = getValueState(I);
+        if (!meet(Old, NewVal))
+            return;                          
+        for (User *U : I->users())
+            if (auto *UI = dyn_cast<Instruction>(U))
+                SSAWorklist.push_back(UI);
+    }
+
+    void markOverdefined(Value *V) {
+        LatticeValue &LV = getValueState(V);
+        if (LV.isOverdefined())
+            return;
+        LV = LatticeValue::makeOverdefined();
+        if (auto *I = dyn_cast<Instruction>(V))
+            for (User *U : I->users())
+                if (auto *UI = dyn_cast<Instruction>(U))
+                    SSAWorklist.push_back(UI);
+    }
+
+    void initialize(Function &F) {
+        for (Argument &Arg : F.args())
+            markOverdefined(&Arg);
+
+        BasicBlock *Entry = &F.getEntryBlock();
+        ExecutableBlocks.insert(Entry);
+        CFGWorklist.push_back({nullptr, Entry});
+    }
 
